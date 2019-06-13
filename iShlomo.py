718654fe83d48fd2ea396fa3fa5e48e6dcc1f8ae
KostyaShey <sheynkler87@gmail.com>
2019-06-13 19:57:35 +0200

    edit income + setup adjustments + create month adjustments
    added two new buttons to edit the income
    added new functionality to setup - the DB will be created  automaticaly on first start
    reworked the createmonth funktion - monthly income and expences are now added to the right month only

--- iShlomo.py
+++ iShlomo.py
@@ -21,10 +21,35 @@ c = conn.cursor()
 
 #creating a new month and adding the monthly values
 def createMonth():
+
+	c.execute('SELECT * FROM MonExpences')
+	monexpences = c.fetchall()
 	c.execute('CREATE TABLE IF NOT EXISTS {month}{year}("ID" TEXT NOT NULL, "Value" INTEGER, "Name" TEXT, "Type" TEXT, PRIMARY KEY("ID"))'\
 		.format(month = MONTH[currentmonth], year = currentyear))
-	c.execute('INSERT OR IGNORE INTO {month}{year} SELECT ID, Value, Name, Type FROM MonExpences'.format(month = MONTH[currentmonth], year = currentyear))
-	c.execute('INSERT OR IGNORE INTO {month}{year} SELECT ID, Value, Name, Type FROM MonIncome'.format(month = MONTH[currentmonth], year = currentyear))
+	index = 1
+	for i in range(len(monexpences)):
+		monthindex = 1
+		for digit in monexpences[i][5]:
+			if int(digit) == 1 and monthindex == currentmonth:
+				c.execute('INSERT OR IGNORE INTO {month}{year} SELECT ID, Value, Name, Type FROM MonExpences WHERE ID = "MEXP{index}"'\
+					.format(month = MONTH[currentmonth], year = currentyear, index = index))
+			else:
+				monthindex += 1
+		index +=1
+	conn.commit()
+
+	c.execute('SELECT * FROM MonIncome')
+	monincome = c.fetchall()
+	index2 = 1
+	for i in range(len(monincome)):
+		monthindex2 = 1
+		for digit in monincome[i][5]:
+			if int(digit) == 1 and monthindex2 == currentmonth:
+				c.execute('INSERT OR IGNORE INTO {month}{year} SELECT ID, Value, Name, Type FROM MonIncome WHERE ID = "MINC{index2}"'\
+					.format(month = MONTH[currentmonth], year = currentyear, index2 = index2))
+			else:
+				monthindex2 += 1
+		index2 +=1
 	conn.commit()
 
 #checks if the current month and year is equal to the first month and year in the db 
@@ -42,16 +67,28 @@ def monthchecker():
 def start():
 	c.execute('CREATE TABLE IF NOT EXISTS "MonExpences" ("ID" TEXT NOT NULL,"Name" TEXT NOT NULL, "Value" INTEGER NOT NULL, "Start" TEXT NOT NULL, "End" TEXT NOT NULL, "Month" TEXT NOT NULL, "Type" TEXT, PRIMARY KEY ("ID"))')
 	c.execute('CREATE TABLE IF NOT EXISTS "MonIncome" ("ID" TEXT NOT NULL,"Name" TEXT NOT NULL, "Value" INTEGER NOT NULL, "Start" TEXT NOT NULL, "End" TEXT NOT NULL, "Month" TEXT NOT NULL, "Type" TEXT, PRIMARY KEY ("ID"))')
+	c.execute('CREATE TABLE IF NOT EXISTS "System" ("ID" INTEGER,"Setup" INTEGER, "FirstMonth" INTEGER, "FirstYear" INTEGER, PRIMARY KEY ("ID"))')
+	c.execute('INSERT OR IGNORE INTO System VALUES (1, 0, 0, 0)')
 	conn.commit()
 
 	c.execute('SELECT * FROM System')
 	booltocheck = c.fetchall()
-	if booltocheck[0][0] == 0:
+	if booltocheck[0][1] == 0:
 		createMonth()
 		c.execute('UPDATE System SET Setup= 1')
 		c.execute('UPDATE System SET FirstMonth = {month}'.format(month = currentmonth))
 		c.execute('UPDATE System SET FirstYear= {year}'.format(year = currentyear))
 		conn.commit()
+
+#use to return the previous month
+def monthminusone(currentm, currenty):
+
+	if currentm == 1:
+		currentm == 12
+		currenty -= 1
+	else:
+		currentm -= 1
+	return currentm, currenty
 
 #fetches all rows in a month
 def fetchmonthdata(currentmonth, currentyear):
@@ -92,16 +129,6 @@ def fetchtotalbalance(monthdata):
 			total -= row[1]
 	return total
 
-#use to return the previous month
-def monthminusone(currentm, currenty):
-
-	if currentm == 1:
-		currentm == 12
-		currenty -= 1
-	else:
-		currentm -= 1
-	return currentm, currenty
-
 def update_text_in_totalbox(monthdata, label, Type):
 	
 	Dictionaryforstring = {"MINC" : "Total income: ", "MEXP" : "Total expences: ", "MONL" : "Money left: ", "BLM" : "Balance from last month: "}
@@ -114,10 +141,12 @@ def update_text_in_totalbox(monthdata, label, Type):
 		label["text"] = Dictionaryforstring[Type] + str(total) + "€"
 	
 	if Type == "BLM":
+		
 		blmtotal = 0
 		if monthchecker():
 			label["text"] = "Balance from last month: -"
 		else:
+			monthdata = fetchmonthdata(monthminusone(currentmonth, currentyear)[0], monthminusone(currentmonth, currentyear)[1])
 			blmtotal = fetchtotalbalance(monthdata)
 			label["text"] = Dictionaryforstring[Type] + str(blmtotal) + " €"
 	
@@ -325,41 +354,29 @@ def openinputwindow(Type):
 		ValueEntry.place(relx = 0.30, rely = 0.09, relwidth = 0.55)
 		
 		JanVar = tk.IntVar()
-		January = tk.Checkbutton(inputframe, text = "January", variable = JanVar, bg = "white")
-		January.place(relx = 0.30, rely = 0.19)
+		January = tk.Checkbutton(inputframe, text = "January", variable = JanVar, bg = "white").place(relx = 0.30, rely = 0.19)
 		FebVar = tk.IntVar()
-		February = tk.Checkbutton(inputframe, text = "February", variable = FebVar, bg = "white")
-		February.place(relx = 0.59, rely = 0.19)
+		February = tk.Checkbutton(inputframe, text = "February", variable = FebVar, bg = "white").place(relx = 0.59, rely = 0.19)
 		MarVar = tk.IntVar()
-		March = tk.Checkbutton(inputframe, text = "March", variable = MarVar, bg = "white")
-		March.place(relx = 0.30, rely = 0.24)
+		March = tk.Checkbutton(inputframe, text = "March", variable = MarVar, bg = "white").place(relx = 0.30, rely = 0.24)
 		AprVar = tk.IntVar()
-		April = tk.Checkbutton(inputframe, text = "April", variable = AprVar, bg = "white")
-		April.place(relx = 0.59, rely = 0.24)
+		April = tk.Checkbutton(inputframe, text = "April", variable = AprVar, bg = "white").place(relx = 0.59, rely = 0.24)
 		MayVar = tk.IntVar()
-		May = tk.Checkbutton(inputframe, text = "May", variable = MayVar, bg = "white")
-		May.place(relx = 0.30, rely = 0.29)
+		May = tk.Checkbutton(inputframe, text = "May", variable = MayVar, bg = "white").place(relx = 0.30, rely = 0.29)
 		JunVar = tk.IntVar()
-		June = tk.Checkbutton(inputframe, text = "June", variable = JunVar, bg = "white")
-		June.place(relx = 0.59, rely = 0.29)
+		June = tk.Checkbutton(inputframe, text = "June", variable = JunVar, bg = "white").place(relx = 0.59, rely = 0.29)
 		JulVar = tk.IntVar()
-		July = tk.Checkbutton(inputframe, text = "July", variable = JulVar, bg = "white")
-		July.place(relx = 0.30, rely = 0.34)
+		July = tk.Checkbutton(inputframe, text = "July", variable = JulVar, bg = "white").place(relx = 0.30, rely = 0.34)
 		AugVar = tk.IntVar()
-		August = tk.Checkbutton(inputframe, text = "August", variable = AugVar, bg = "white")
-		August.place(relx = 0.59, rely = 0.34)
+		August = tk.Checkbutton(inputframe, text = "August", variable = AugVar, bg = "white").place(relx = 0.59, rely = 0.34)
 		SepVar = tk.IntVar()
-		September = tk.Checkbutton(inputframe, text = "September", variable = SepVar, bg = "white")
-		September.place(relx = 0.30, rely = 0.39)
+		September = tk.Checkbutton(inputframe, text = "September", variable = SepVar, bg = "white").place(relx = 0.30, rely = 0.39)
 		OctVar = tk.IntVar()
-		October = tk.Checkbutton(inputframe, text = "October", variable = OctVar, bg = "white")
-		October.place(relx = 0.59, rely = 0.39)
+		October = tk.Checkbutton(inputframe, text = "October", variable = OctVar, bg = "white").place(relx = 0.59, rely = 0.39)
 		NovVar = tk.IntVar()
-		November = tk.Checkbutton(inputframe, text = "November", variable = NovVar, bg = "white")
-		November.place(relx = 0.30, rely = 0.44)
+		November = tk.Checkbutton(inputframe, text = "November", variable = NovVar, bg = "white").place(relx = 0.30, rely = 0.44)
 		DecVar = tk.IntVar()
-		December = tk.Checkbutton(inputframe, text = "December", variable = DecVar, bg = "white")
-		December.place(relx = 0.59, rely = 0.44)
+		December = tk.Checkbutton(inputframe, text = "December", variable = DecVar, bg = "white").place(relx = 0.59, rely = 0.44)
 
 		Placeholder1Label = tk.Label(inputframe, text="Placeholder for start date input", bg = "grey")
 		Placeholder1Label.place(relx = 0.05, rely = 0.55, relwidth = 0.90)
@@ -382,10 +399,59 @@ def openinputwindow(Type):
 
 	inputwindow.mainloop()
 
+def fetchrowfrommonth(Type, Listwithrows, NameEntry, ValueEntry):
+	ID = Listwithrows.curselection()[0] + 1
+	c.execute('SELECT * FROM {month}{year} WHERE ID = "{TYpe}{id}"'.format(month = MONTH[currentmonth], year = currentyear, TYpe = Type, id = ID))
+	row = c.fetchall()
+	NameEntry.delete(0, tk.END)
+	NameEntry.insert(tk.END, row[0][2])
+	ValueEntry.delete(0, tk.END)
+	ValueEntry.insert(tk.END, row[0][1])
+
+def saverowfrommonth(Type, Listwithrows, NameEntry, ValueEntry, editwindow):
+	ID = Listwithrows.curselection()[0] + 1
+	c.execute('UPDATE {month}{year} SET Name = "{NameValue}", Value = "{ValueValue}" WHERE ID = "{TYpe}{id}"'\
+		.format(month = MONTH[currentmonth], year = currentyear, NameValue = NameEntry.get(), ValueValue = ValueEntry.get(), TYpe = Type, id = ID))
+	conn.commit()
+	close_window(editwindow)
+	update_textbox(currentmonth, currentyear)
+	update_totalbox(currentmonth, currentyear)
 
 def openeditwindow(Type):
 	editwindow = tk.Toplevel()
 	editwindow.title("Edit "+ TYPEOFEXPENCES[Type])
+
+	canvas = tk.Canvas(editwindow, height = HEIGHT, width = WIDTH/4)
+	canvas.pack()
+
+	editframe = tk.Frame(editwindow, bg = "white")
+	editframe.place(relx = 0.05, rely = 0.05, relwidth = 0.9, relheight = 0.9)
+
+	monthdata = fetchmonthdata(currentmonth, currentyear)
+
+	Listwithrows = tk.Listbox(editframe, selectmode = "single")
+	index = 1
+	for row in monthdata:
+		if row[3] == Type:
+			Listwithrows.insert(index, row[2])
+			index +=1
+
+	Listwithrows.place(relx = 0.05, rely = 0.03, relwidth = 0.9, relheight = 0.3)
+
+	NameLabel = tk.Label(editframe, text="Name:", bg = "white")
+	NameLabel.place(relx = 0.05, rely = 0.43, relwidth = 0.30)
+	NameEntry = tk.Entry(editframe, bd =5)
+	NameEntry.place(relx = 0.30, rely = 0.43, relwidth = 0.55)
+	ValueLabel = tk.Label(editframe, text="Value:", bg = "white")
+	ValueLabel.place(relx = 0.05, rely = 0.49, relwidth = 0.30)
+	ValueEntry = tk.Entry(editframe, bd =5)
+	ValueEntry.place(relx = 0.30, rely = 0.49, relwidth = 0.55)
+
+	fetchrow = partial(fetchrowfrommonth, Type, Listwithrows, NameEntry, ValueEntry)
+	selectbutton = tk.Button(editframe, text = "Select", command = fetchrow).place(relx = 0.05, rely = 0.35, relheight = RELBUTTONHEIGHT, relwidth = 0.9)
+
+	saverow = partial(saverowfrommonth, Type, Listwithrows, NameEntry, ValueEntry, editwindow)
+	savebutton = tk.Button(editframe, text = "Save changes", command = saverow).place(relx = 0.05, rely = 0.55, relheight = RELBUTTONHEIGHT, relwidth = 0.9)
 
 	editwindow.mainloop()
 
@@ -409,24 +475,22 @@ buttonframe = tk.Frame(root, bg = "white")
 buttonframe.place(relx = 0.05, rely = 0.05, relwidth = 0.15, relheight = 0.9)
 
 openmonthincome = partial(openinputwindow, "MINC")
-addmonthincome = tk.Button(buttonframe, text = "Add monthly income", command = openmonthincome)
-addmonthincome.place(relx = 0.05, rely = 0.01, relheight = RELBUTTONHEIGHT, relwidth = 0.9)
+addmonthincome = tk.Button(buttonframe, text = "Add monthly income", command = openmonthincome).place(relx = 0.05, rely = 0.01, relheight = RELBUTTONHEIGHT, relwidth = 0.9)
 
 openincome = partial(openinputwindow, "INC")
-addincome = tk.Button(buttonframe, text = "Add income", command = openincome)
-addincome.place(relx = 0.05, rely = 0.07, relheight = RELBUTTONHEIGHT, relwidth = 0.9)
+addincome = tk.Button(buttonframe, text = "Add income", command = openincome).place(relx = 0.05, rely = 0.07, relheight = RELBUTTONHEIGHT, relwidth = 0.9)
 
 openmonthexpence = partial(openinputwindow, "MEXP")
-addmonthexpence = tk.Button(buttonframe, text = "Add month expence", command = openmonthexpence)
-addmonthexpence.place(relx = 0.05, rely = 0.13, relheight = RELBUTTONHEIGHT, relwidth = 0.9)
+addmonthexpence = tk.Button(buttonframe, text = "Add month expence", command = openmonthexpence).place(relx = 0.05, rely = 0.13, relheight = RELBUTTONHEIGHT, relwidth = 0.9)
 
 openexpence = partial(openinputwindow, "EXP")
-addexpence = tk.Button(buttonframe, text = "Add expence", command = openexpence)
-addexpence.place(relx = 0.05, rely = 0.19, relheight = RELBUTTONHEIGHT, relwidth = 0.9)
+addexpence = tk.Button(buttonframe, text = "Add expence", command = openexpence).place(relx = 0.05, rely = 0.19, relheight = RELBUTTONHEIGHT, relwidth = 0.9)
 
-#openeditexpence = partial(openeditwindow, "EXP")
-editexpences = tk.Button(buttonframe, text = "Edit expences", command = openexpence)
-editexpences.place(relx = 0.05, rely = 0.27, relheight = RELBUTTONHEIGHT, relwidth = 0.9)
+openeditexpence = partial(openeditwindow, "EXP")
+editexpences = tk.Button(buttonframe, text = "Edit expences", command = openeditexpence).place(relx = 0.05, rely = 0.27, relheight = RELBUTTONHEIGHT, relwidth = 0.9)
+
+openeditincome = partial(openeditwindow, "INC")
+editexpences = tk.Button(buttonframe, text = "Edit income", command = openeditincome).place(relx = 0.05, rely = 0.33, relheight = RELBUTTONHEIGHT, relwidth = 0.9)
 
 
 #frame for the results:
@@ -441,8 +505,7 @@ if monthchecker():
 datelabel = tk.Label(textframe, bg = "#d9d9d9", bd = 10, justify='center', text = dateforlabel())
 datelabel.place(relx = 0.5, rely = 0.01, relwidth=0.2, relheight=0.05, anchor = "n")
 
-nextmonth = tk.Button(textframe, text = ">", command = buttonnextmonth)
-nextmonth.place(relx = 0.63, rely = 0.01, relwidth=0.04, relheight=0.05, anchor = "n")
+nextmonth = tk.Button(textframe, text = ">", command = buttonnextmonth).place(relx = 0.63, rely = 0.01, relwidth=0.04, relheight=0.05, anchor = "n")
 
 textbox = tk.Text(textframe, bg = "white")
 textbox.place(rely = 0.07, relwidth=1, relheight=0.93)
